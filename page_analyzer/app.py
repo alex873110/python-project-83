e1013edfc94c39c06c15deb71daa02b86651ef4a
Alexander <alexzhur87@mail.ru>
2024-01-26 10:09:20 +0300

    correct app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
 # from flask import request, flash, redirect
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
 @app.route('/')
