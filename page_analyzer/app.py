4eb668259e43393fa07014a11c3edba85f24dd3c
Alexander <alexzhur87@mail.ru>
2024-03-21 23:28:20 +0300

    correct app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -70,7 +70,7 @@ def url_add():
             id = cur.fetchone()[0]
         conn.close()
 #    flash('Страница успешно добавлена', 'success')
-    return redirect(url_for('url_info', id=id))
+        return redirect(url_for('url_info', id=id))
 
 
 @app.get('/urls/<id>')
