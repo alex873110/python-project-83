5acd61668b5f8b62cd00dae8e35dce3b9b582e70
Alexander <alexzhur87@mail.ru>
2024-03-26 19:19:05 +0300

    correct app.py add title, h1, description

--- page_analyzer/html_parser.py
+++ page_analyzer/html_parser.py
@@ -4,8 +4,9 @@ from bs4 import BeautifulSoup
 def get_seo(content):
     content = BeautifulSoup(content, 'html.parser')
     title = content.title
-    title = title.string if title else None
-    h1 = content.h1.text if content.h1 else None
+    title = title.string if title else ''
+    h1 = content.h1.text if content.h1 else ''
     description = content.find("meta", attrs={"name": "description"})
-    description = description.get("content", None) if description else None
+    description = description.get("content", None) if description else ''
+    description = description if len(description) < 255 else description[:252] + '...'
     return {"title": title, "h1": h1, "description": description}
