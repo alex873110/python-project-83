9045b2b72656f1d7ba58dfda7f25ba22b6854fc1
Alexander <alexzhur87@mail.ru>
2024-03-27 16:35:02 +0300

    correct app.py add title, h1, description

--- page_analyzer/html_parser.py
+++ page_analyzer/html_parser.py
@@ -6,7 +6,7 @@ def get_seo(content):
     title = content.title
     title = title.string if title else ''
     h1 = content.h1.text if content.h1 else ''
-    description = content.find('meta', {'name': 'description'})
-    description = description['content'] if description else ''
+    description = content.find("meta", attrs={"name": "description"})
+    description = description["content"] if description else ''
     description = description if len(description) < 255 else description[:252] + '...'
     return {"title": title, "h1": h1, 'description': description}
