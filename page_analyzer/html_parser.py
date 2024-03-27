dda62bb0f1ca55e229ceec8ec17314c7e3ac50f8
Alexander <alexzhur87@mail.ru>
2024-03-27 20:47:48 +0300

    correct app.py

--- page_analyzer/html_parser.py
+++ page_analyzer/html_parser.py
@@ -6,7 +6,7 @@ def get_seo(content):
     title = soup.title
     title = title.string if title else ''
     h1 = soup.h1.text if soup.h1 else ''
-    description_tag = soup.find('meta', {name: 'description'})
+    description_tag = soup.find('meta', {'name': 'description'})
     description = description_tag['content'] if description_tag else ''
     description = description if len(description) < 255 else description[:252] + '...'
     return title, h1, description
