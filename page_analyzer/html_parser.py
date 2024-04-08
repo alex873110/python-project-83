641e9eee81574a01da0c7f3da3b80088235ed2e9
Alexander <83303473+alex873110@users.noreply.github.com>
2024-04-08 08:54:55 +0300

    Update html_parser.py

--- page_analyzer/html_parser.py
+++ page_analyzer/html_parser.py
@@ -5,13 +5,10 @@ def get_seo_data(content):
     soup = BeautifulSoup(content, 'html.parser')
     title = soup.title
     title = title.string if title else ''
-    if len(title) >= 255:
-        title = title[:252] + '...'
+    title = title[:255]
     h1 = soup.h1.text if soup.h1 else ''
-    if len(h1) >= 255:
-        h1 = h1[:252] + '...'
+    h1 = h1[:255]
     description_tag = soup.find("meta", attrs={'name': "description"})
     description = description_tag['content'] if description_tag else ''
-    if len(description) >= 255:
-        description = description[:252] + '...'
+    description = description[:255]
     return title, h1, description
